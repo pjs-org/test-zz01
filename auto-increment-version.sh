7a912a2333d609c4e6be4d385d0ceee995f4ad5d
jalapr01 <98925405+jalapr01@users.noreply.github.com>
2023-06-08 11:56:00 -0700

    Update auto-increment-version.sh

--- auto-increment-version.sh
+++ auto-increment-version.sh
@@ -19,22 +19,22 @@ MAJOR=`git log --format=%B -n 1 HEAD | grep 'breaking'`
 MINOR=`git log --format=%B -n 1 HEAD | grep 'feat'`
 
 if [ "$MAJOR" ]; then
-    echo "Update major version"
+    #echo "Update major version"
     VNUM1=$((VNUM1+1))
     VNUM2=0
     VNUM3=0
 elif [ "$MINOR" ]; then
-    echo "Update minor version"
+    #echo "Update minor version"
     VNUM2=$((VNUM2+1))
     VNUM3=0
 else
-    echo "Update patch version"
+    #echo "Update patch version"
     VNUM3=$((VNUM3+1))
 fi
 
 NEW_TAG=$VNUM1.$VNUM2.$VNUM3-SNAPSHOT 
 
 #create new tag
-echo "NEW_TAG=$VNUM1.$VNUM2.$VNUM3-SNAPSHOT" >> $GITHUB_OUTPUT
+echo "NEW_TAG=$VNUM1.$VNUM2.$VNUM3-SNAPSHOT" 
 
 echo "Updating $VERSION to $NEW_TAG"
